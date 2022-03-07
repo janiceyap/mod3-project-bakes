7a501974b675998f23569c9c99c129e739c98e57
m-chocolate <94664208+m-chocolate@users.noreply.github.com>
2022-03-07 23:46:35 +0800

    added validations for model and logic for updatePurchase

--- model/purchasehistories.model.js
+++ model/purchasehistories.model.js
@@ -24,7 +24,11 @@ module.exports = function (sequelize) {
             purchaseDate: {
                 type: DataTypes.DATE,
                 allowNull: false,
-                field: 'purchase_date'
+                field: 'purchase_date',
+                validate: {
+                isAfter: "2022-03-01",
+                isBefore: "2022-12-31",
+                }
             },
             subtotal: {
                 type: DataTypes.DECIMAL(10,2),
@@ -44,17 +48,38 @@ module.exports = function (sequelize) {
             paymentMethod : {
                 type: DataTypes.STRING(100),
                 allowNull: false,
-                field: 'payment_method'
+                field: 'payment_method',
+                validate: {
+                    isIn: {
+                    args: [['VISA','MASTERCARD']],
+                    msg: 'ACCEPT ONLY VISA OR MASTERCARD',
+                    }
+                }
             },
             paymentTxnId : {
                 type: DataTypes.STRING(100),
                 allowNull: false,
-                field: 'payment_txn_id'
+                field: 'payment_txn_id',
+                validate: {
+                    len: [12,18],
+                    isAlphanumeric: true,
+                    isUppercase: true,
+                }
             },
             invoiceId : {
                 type: DataTypes.STRING(100),
                 allowNull: false,
                 field: 'invoice_id',
+                validate: {
+                    customValidator(value) {
+                        if (!value.startsWith('SB') ) {
+                          throw new Error("Invoice needs to start with 'SB' ");
+                        }
+                    },
+                    isAlphanumeric: true,
+                    isUppercase: true,
+                    len: [6,8],  
+                }
             },
             createdAt: {
                 type: DataTypes.DATE,
--- services/purchasehistories.services.js
+++ services/purchasehistories.services.js
@@ -1,4 +1,5 @@
 const { PurchaseHistories, User } = require( "../model/model" );
+let gst = 0.07;
 
 module.exports = {
 
@@ -61,8 +62,8 @@ module.exports = {
             return result;
         }
 
-
     },
+
     deletePurchase: async (purchaseId) =>{
         let result = {
             message: null,
@@ -86,39 +87,51 @@ module.exports = {
         return result;
     },
 
-    updatePurchase: async (updateDetails) => {
+    updatePurchase: async (update) => {
         let result = {
             message: null,
             status: null,
             data: null,
         }
 
-        const purchase = await PurchaseHistories.findByPk(updateDetails.purchaseId);
+        const purchase = await PurchaseHistories.findByPk(update.purchaseId);
 
         if (!purchase) {
-            result.message = `Purchase is not found`;
+            result.message = `Purchase is not found. Purchase id`;
             result.status = 400;
             return result;
         }
 
-        //to add logic for partial update
-        purchase.id = updateDetails.purchaseId;
-        purchase.userId = updateDetails.userId;
-        purchase.recipeId = updateDetails.recipeId;
-        purchase.purchaseDate = updateDetails.purchaseDate;
-        purchase.subtotal = updateDetails.subtotal;
-        purchase.gst = updateDetails.gst;
-        purchase.total = updateDetails.total;
-        purchase.paymentTxnId = updateDetails.paymentTxnId;
-        purchase.paymentMethod = updateDetails.paymentMethod;
-        purchase.invoiceId = updateDetails.invoiceId;
-        await purchase.save();
+        //selective update
+        try {
+
+            if (update.userId) purchase.userId = update.userId;
+            if (update.recipeId) purchase.recipeId = update.recipeId;
+            if (update.purchaseDate) purchase.purchaseDate = update.purchaseDate;
+            if (update.subtotal) {
+                purchase.subtotal = update.subtotal;
+                purchase.gst = purchase.subtotal*gst;
+                purchase.total = purchase.subtotal + purchase.gst;
+                //postman may display many D.P, database only keep 2 decimal place
+                }   
+
+            if (update.paymentTxnId) purchase.paymentTxnId = update.paymentTxnId;
+            if (update.paymentMethod) purchase.paymentMethod = update.paymentMethod;
+            if (update.invoiceId) purchase.invoiceId = update.invoiceId;
+
+            await purchase.save( { validate: true });
+
+        } catch(err) {
+            console.log(err);
+            result.message = `Oops! Update for ${update.purchaseId} didn't go through, ${err}`;
+            result.status = 400;
+            return result;
+        }
+
         result.data = purchase;
         result.status = 200;
-        result.message = "Update Purchase History successful!";
+        result.message = `Update Purchase id: ${update.purchaseId} successful!`;
         return result;
-
-
 
     }
 
