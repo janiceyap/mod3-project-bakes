2badff88bfa648e5282309116d8f63246ea02e46
Michelle Tay <mich.tay.elle@gmail.com>
2022-03-09 11:14:00 +0800

    Update recipeSteps.joi.js

--- middleware/recipeSteps.joi.js
+++ middleware/recipeSteps.joi.js
@@ -5,6 +5,8 @@ module.exports = {
     inputSteps: async(req, res, next) => {
 
         const stepsSchema = Joi.object({
+            userId: Joi.number(),
+            recipeId: Joi.number(),
             steps: Joi.string()
                 .alphanum()
                 .min(50)
@@ -12,6 +14,18 @@ module.exports = {
                 .required(),
         })
 
-        stepsSchema.validate({});
+        const {error, value} = stepsSchema.validate(req.body);
+        // stepsSchema.validate({});
+        console.log("Upload Steps Schema Validate Error", error);
+        console.log("Upload Steps Schema Validate Value", value);
+
+        if(error){
+
+            res.status(400).json({
+                message: 'Invalid request data.',
+                error: error.message,
+            });
+        }
+        next();
     }
 };
